a190b9dfc117cf0130802ce303f920d31da76779
Avinash <avinash_kumar3@epam.com>
2025-10-10 08:53:55 +0530

    commit 9/10/2025

---
+++ src/ex_01_pythonBasics/Lab_005_Print function.py
@@ -0,0 +1,7 @@
+# Learning print function
+print("Avinash", "Kumar", 123, "Learning", "Python", True, 456, 3.145, sep='-@-', end='\n')
+print("Avinash", "Kumar", 123, "Learning", "Python", True, 456, 3.145, sep='-@-', end='_')
+print("Avinash", "Kumar", 123, "Learning", "Python", True, 456, 3.145, sep='-@-')
+print("Avinash", "Kumar", 123, "Learning", "Python", True, 456, 3.145, sep='-@-')
+print("Avinash", "Kumar", 123, "Learning", "Python", True, 456, 3.145, sep='-@-', end='***')
+# Print() - Case sensitive
---
+++ src/ex_02_keywords_identifier_variables/Lab_006_Variables.py
@@ -0,0 +1,9 @@
+#leanring variables
+name = "Avinash"
+age = 25.67
+string = "Kumar"
+shopping_list = ["milk","bread","butter"]
+_ = "Underscore"
+#2_name = "name"
+Alpa234 = "Alphanumeric"
+print(age, name, string, shopping_list,_,Alpa234, sep=' * ', end='\n', file=None)
