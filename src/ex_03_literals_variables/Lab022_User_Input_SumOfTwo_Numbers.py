c485341b2727279d06737c4cdfb37cb203b7dd65
Avinash <avinash_kumar3@epam.com>
2025-10-12 22:34:09 +0530

    commit 11/10/2025

---
+++ src/ex_03_literals_variables/Lab022_User_Input_SumOfTwo_Numbers.py
@@ -0,0 +1,12 @@
+# Logic buliding
+#Step1
+#I/P = num1, num2 (int)
+#O/P = sum, mul, div, sub (always ask from the interviewer), float
+
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
+# Output
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
