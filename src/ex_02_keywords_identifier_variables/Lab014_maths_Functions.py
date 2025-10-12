d9cede5caff4d9b028073eabca317c653d0be397
Avinash <avinash_kumar3@epam.com>
2025-10-12 17:45:00 +0530

    commit 11/10/2025

---
+++ src/ex_02_keywords_identifier_variables/Lab014_maths_Functions.py
@@ -0,0 +1,10 @@
+#To find the max , min - functions are available - prebuilt function
+#Max number between 3,4
+result = max(3,4)
+print(result)
+result = min(3,4)
+print(result)
+result = max(3,4,5,6,7,89,100)
+print(result)
+result = min(3,4,5,6,-1,0,1,89,100)
+print(result)
