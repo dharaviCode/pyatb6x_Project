d2706248528174ffbe2f765f8e777f1f4854472e
Avinash <avinash_kumar3@epam.com>
2025-10-12 16:42:12 +0530

    commit 11/10/2025

---
+++ src/ex_02_keywords_identifier_variables/Lab007_Variables_Names.py
@@ -0,0 +1,17 @@
+#123abc = 90 -- not a valid variable name
+#Type function
+_name = "Avinash"
+print(type(_name)) #class str
+_abc = 23
+print(type(_abc)) #class int
+pi = 3.14
+print(type(pi))  #class float
+isMale = True
+print(type(isMale))  #class bool
+
+#complexNumber data_Type (Will be used in DataScience topic but will not be used in Automation)
+complex_number = 2+3j
+print(complex_number)
+print(type(complex_number))  #class complex
+print(complex_number.real) #value for real number
+print(complex_number.imag)  #value for imaginary number
