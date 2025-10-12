7f22c2b74ee4ec9e5aef7a145874d0e09f40d4f0
Avinash <avinash_kumar3@epam.com>
2025-10-12 22:14:48 +0530

    commit 11/10/2025

---
+++ src/ex_03_literals_variables/Lab021_UserInput.py
@@ -0,0 +1,7 @@
+# How to take the user input
+
+name = input("Enter your name?") # It takes input only as a str
+# Read a name from a standard input
+print("Your name is:", name)
+print(type(name)) # Provide output as a str
+
