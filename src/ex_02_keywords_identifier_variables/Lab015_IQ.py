d8f0889e298cde6490652886f7fdae083ae9ccf9
Avinash <avinash_kumar3@epam.com>
2025-10-12 17:56:28 +0530

    commit 11/10/2025

---
+++ src/ex_02_keywords_identifier_variables/Lab015_IQ.py
@@ -0,0 +1,6 @@
+#print("hello world" + 15) #TypeError: can only concatenate str (not "int") to str
+
+#In Python there is no concept of concatenation
+#If you want to print you need to convert all variables into String(str)
+print("hello world" + str(15))
+print("hello world" + 15) #TypeError: can only concatenate str (not "int") to str
