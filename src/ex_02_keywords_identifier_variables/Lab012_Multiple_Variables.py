a7bf4498f96e59813339c24fcbe8c5d5a050f440
Avinash <avinash_kumar3@epam.com>
2025-10-12 17:32:47 +0530

    commit 11/10/2025

---
+++ src/ex_02_keywords_identifier_variables/Lab012_Multiple_Variables.py
@@ -0,0 +1,13 @@
+# We can assign multiple variables at a time by separating it with ,
+
+a , b , c = 23 , 24 , 25
+print(a)
+print(b)
+print(c)
+print(a,b,c, sep=' | ')
+
+a , b , c , d = 23 , 24.5 , "number" , True
+print(a)
+print(b)
+print(c)
+print(d)
