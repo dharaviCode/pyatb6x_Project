129e0e967371ce58235797d5c83fd83c30a23ae1
Avinash <avinash_kumar3@epam.com>
2025-10-14 15:12:09 +0530

    commit 14/10/2025

---
+++ src/task/Task004_14thOct.py
@@ -0,0 +1,14 @@
+# Write a Python program to calculate the area of a triangle using the formula
+# Area = 1/2 * base * height
+
+#Step1
+#I/P, Logic building
+base = float(input("Enter a base of triangle: "))
+height = float(input("Enter a height of triangle: "))
+
+#Logic building
+area = 1/2*(base * height)
+#print(1/2)
+
+#O/P
+print("Area of triangle is:", area)
---
+++ src/task/Task005_14thOct.py
@@ -0,0 +1,11 @@
+# Write a program to check if a number entered by the user is greater than 100.
+
+# I/O of number is taken
+num1 = float(input("Enter a number: "))
+
+# Logic building
+result =  "entered number greater than 100" if num1 > 100 else "entered number less than 100"
+
+# O/P print
+print(result)
+
