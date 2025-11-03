242470f40459ed1e2d0e9db17368b762c1aead66
Avinash <avinash_kumar3@epam.com>
2025-11-03 20:38:05 +0530

    commit 28/10/2025

---
+++ src/task/Task013_28thOct_Even_numbers.py
@@ -0,0 +1,11 @@
+# Print even numbers between 1 and 20
+
+for i in range(1,21):
+    if i % 2 == 0:
+        print("Even number", i)
+
+"""
+# Optimized code
+for num in range(2, 21, 2):
+    print(num)
+    """
