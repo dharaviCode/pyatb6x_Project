3a979ccc11b0970c7a2980ef02666796e74c58af
Avinash <avinash_kumar3@epam.com>
2025-11-25 20:31:47 +0530

    commit 6/11/2025

---
+++ src/ex_13_List/Lab096_List.py
@@ -0,0 +1,13 @@
+# List - Collection  of items
+# grocery List - butter, bread, banana, paneer.
+# 10th marks - 90,91,92, 78, 56
+
+my_list = [1,2,3,4,5]  # Same type of data (int)
+my_list2 = [1,"Avi",True, 12.34]  # different type of data (int)
+print(my_list)
+print(my_list2)
+print(type(my_list))
+print(len(my_list))
+print(my_list[0])
+print(my_list2[1])
+print(my_list[6]) # IndexError: list index out of range
---
+++ src/ex_14_Tuple/Lab099_Tuple.py
@@ -0,0 +1,12 @@
+my_tuple = (1,2,3,4,5)
+print(my_tuple)
+# my_tuple[0] = 12 # TypeError: 'tuple' object does not support item assignment
+
+# With Mixed Data Types:
+info = ("Avi", 34, True, 9.8)
+print(info)
+
+#Tuple with One Element
+single=  (3,)
+print(type(single)) # <class 'tuple'>
+
