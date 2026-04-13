25649ee59ee77cb18dfa5db751ea0d1fb263ddf9
Avinash <avinash_kumar3@epam.com>
2026-04-13 11:30:22 +0530

    commit 13/04/2026

---
+++ src/ex_01_pythonBasics/Prac_003_Comments.py
@@ -0,0 +1,15 @@
+# This is the comment with # in it used in python which is called inline comment
+
+print("Avinash")
+
+# This is the comment with # in it used in python which is called inline comment
+
+# Multiple line comments (""" content """)
+
+print("start of the multiple line comments")
+
+"""
+this is the multiple line comment
+which can be kept under semicolumns 
+by providing the multiple line
+"""
---
+++ src/ex_01_pythonBasics/Prac_004_Variables.py
@@ -0,0 +1,13 @@
+# Underscore can be used as a variable name
+_ = 21
+print(_)
+
+#variable name cannot start with numbers
+"""
+123abc = 21
+print(123abc)
+"""
+
+#variable name can be alphanumeric
+abc123 = 21
+print(abc123)
